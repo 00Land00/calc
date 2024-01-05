9f5dcedd6a5df631d4e04c9b73c9d3a502d3283d
00Land00 <lander.vitug@gmail.com>
2024-01-05 16:34:23 +0800

    add typing of numbers and operators

--- index.js
+++ index.js
@@ -3,12 +3,37 @@ const index = (() => {
 
   const module = {};
 
-  const inputText = document.querySelector("#input");
+  const container = document.querySelector("#input");
+  const inputText = document.querySelector("#text");
   const cursorText = document.querySelector("#cursor");
 
+  const validKeys = /[\d\*\+\-\/]/;
+
+  const typingEH = (e) => {
+    if (e.key.length === 1 && e.key.match(validKeys)) {
+      inputText.innerHTML += e.key;
+    }
+
+    if (e.key === "Backspace" && inputText.innerHTML.length > 0) {
+      inputText.innerHTML = inputText.innerHTML.substring(
+        0,
+        inputText.innerHTML.length - 1
+      );
+    }
+  };
+
   window.addEventListener("DOMContentLoaded", () => {
-    inputText.appendChild(cursorText);
+    window.addEventListener("keydown", typingEH);
   });
 
   return module;
 })();
+
+/* 
+
+- let's get displaying each keystroke as being displayed above
+- let's get the actual calculator logic working
+- let's address the error handling
+- let's add decimal points to the mix
+
+*/

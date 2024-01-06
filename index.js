9fb15f0518434fb236e2d8d1519fb47553736cba
00Land00 <lander.vitug@gmail.com>
2024-01-07 07:23:37 +0800

    implement visible scrolling overflow effect

--- index.js
+++ index.js
@@ -6,15 +6,21 @@ const index = (() => {
   const container = document.querySelector("#input");
   const inputText = document.querySelector("#text");
   const cursorText = document.querySelector("#cursor");
+  const child = document.querySelector("#child");
 
   const validKeys = /[\d\*\+\-\/]/;
+  let childCount = child.childElementCount;
+  let curScroll = 1;
 
-  const typingEH = (e) => {
-    if (e.key.length === 1 && e.key.match(validKeys)) {
-      inputText.innerHTML += e.key;
-    }
+  const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
-    if (e.key === "Backspace" && inputText.innerHTML.length > 0) {
+  module.setScroll = (val) => {
+    curScroll = clamp(val, 1, childCount);
+    child.style.transform = `translate(0, calc(${curScroll * 5}rem - 100%))`;
+  };
+
+  module.backspaceCheck = (key) => {
+    if (key === "Backspace" && inputText.innerHTML.length > 0) {
       inputText.innerHTML = inputText.innerHTML.substring(
         0,
         inputText.innerHTML.length - 1
@@ -22,8 +28,40 @@ const index = (() => {
     }
   };
 
+  module.validKeyCheck = (key) => {
+    if (key.length === 1 && key.match(validKeys)) {
+      // i would put the text up here
+      inputText.innerHTML += key;
+    }
+  };
+
+  module.controlsCheck = (key) => {
+    if (key === "ArrowUp") {
+      curScroll++;
+      module.setScroll(curScroll);
+    }
+    if (key === "ArrowDown") {
+      curScroll--;
+      module.setScroll(curScroll);
+    }
+  };
+
+  const scrollEH = (e) => {
+    let y = e.deltaY;
+    curScroll += y;
+
+    module.setScroll(curScroll);
+  };
+
+  const typingEH = (e) => {
+    module.validKeyCheck(e.key);
+    module.backspaceCheck(e.key);
+    module.controlsCheck(e.key);
+  };
+
   window.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("keydown", typingEH);
+    window.addEventListener("wheel", scrollEH);
   });
 
   return module;
@@ -32,6 +70,13 @@ const index = (() => {
 /* 
 
 - let's get displaying each keystroke as being displayed above
+  - this is the hardest part
+  - how do we get the flexbox to make it like that
+  - i also want it to stick when you scroll
+  - and only move down when i scroll up
+  - flexbox is sized to the content
+
+
 - let's get the actual calculator logic working
 - let's address the error handling
 - let's add decimal points to the mix

14415ce754b9624cfcb1a76e2b53822134459b66
00Land00 <lander.vitug@gmail.com>
2024-05-08 15:24:32 -0400

    complete the display to scroll properly

--- index.js
+++ index.js
@@ -3,37 +3,43 @@ const index = (() => {
 
   const module = {};
 
-  const container = document.querySelector("#input");
-  const inputText = document.querySelector("#text");
-  const cursorText = document.querySelector("#cursor");
-  const child = document.querySelector("#child");
+  const inputText = document.querySelector("#input");
 
   const validKeys = /[\d\*\+\-\/]/;
-  let childCount = child.childElementCount;
+  let inputCount = inputText.childElementCount;
   let curScroll = 1;
 
   const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
+  const scrollToId = (val) => {
+    let scrollVal = val - 1;
+    return inputCount - (scrollVal + 1);
+  };
+
   module.setScroll = (val) => {
-    curScroll = clamp(val, 1, childCount);
-    child.style.transform = `translate(0, calc(${curScroll * 5}rem - 100%))`;
+    curScroll = clamp(val, 1, inputCount);
+    let offset = `${curScroll * 5}rem`;
+    offset += ` + ${Math.max(curScroll - 1, 0) * 5}vh`;
+    inputText.style.transform = `translate(0, calc((${offset}) - 100%))`;
+    // curScroll => is how we get the list of 
+    inputText.children[scrollToId(curScroll)].style.backgroundColor = `yellow`;
   };
 
-  module.backspaceCheck = (key) => {
-    if (key === "Backspace" && inputText.innerHTML.length > 0) {
-      inputText.innerHTML = inputText.innerHTML.substring(
-        0,
-        inputText.innerHTML.length - 1
-      );
-    }
-  };
+  // module.backspaceCheck = (key) => {
+  //   if (key === "Backspace" && inputText.innerHTML.length > 0) {
+  //     inputText.innerHTML = inputText.innerHTML.substring(
+  //       0,
+  //       inputText.innerHTML.length - 1
+  //     );
+  //   }
+  // };
 
-  module.validKeyCheck = (key) => {
-    if (key.length === 1 && key.match(validKeys)) {
-      // i would put the text up here
-      inputText.innerHTML += key;
-    }
-  };
+  // module.validKeyCheck = (key) => {
+  //   if (key.length === 1 && key.match(validKeys)) {
+  //     // i would put the text up here
+  //     inputText.innerHTML += key;
+  //   }
+  // };
 
   module.controlsCheck = (key) => {
     if (key === "ArrowUp") {
@@ -54,8 +60,8 @@ const index = (() => {
   };
 
   const typingEH = (e) => {
-    module.validKeyCheck(e.key);
-    module.backspaceCheck(e.key);
+    // module.validKeyCheck(e.key);
+    // module.backspaceCheck(e.key);
     module.controlsCheck(e.key);
   };
 
@@ -69,13 +75,11 @@ const index = (() => {
 
 /* 
 
-- let's get displaying each keystroke as being displayed above
-  - this is the hardest part
-  - how do we get the flexbox to make it like that
-  - i also want it to stick when you scroll
-  - and only move down when i scroll up
-  - flexbox is sized to the content
-
+- i want the focus to increase in size and smoothly increase or decrease
+  as it enters focus
+- then i have to get the cursor to follow the focus
+- and have it finally add every valid keypress and ensure it doesn't
+  break when i update the childCount
 
 - let's get the actual calculator logic working
 - let's address the error handling

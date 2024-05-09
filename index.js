696db7f918742bcd4f6aea32cbd6fe0434b858f1
Lander Joshua Vitug <lander.vitug@gmail.com>
2024-05-09 19:41:11 -0400

    add error html display and planning

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const index = (() => {
 
   const module = {};
 
-  const historyCapacity = 15;
+  const historyCapacity = 50;
   const historyContainer = document.querySelector("#input");
+  // make reference to the error message
 
-  const validKeys = /[\d\*\+\-\/]/;
+  const validKeys = /[\d\*\+\-\/\.]/;
+  const validOperations = /[\*\+\-\/]/;
+  const validDigit = /^(\d*\.?\d+|\d+\.?\d*)$/;
+  const computableForm = /^(\d*\.?\d+|\d+\.?\d*)[+\-*/](\d*\.?\d+|\d+\.?\d*)$/;
   const cursor = `<span id="cursor">_</span>`;
 
   let recordCount = historyContainer.childElementCount;
@@ -56,7 +60,7 @@ const index = (() => {
 
   const delAbove = () => {
     if (recordCount > historyCapacity) {
-      const delId = 0;
+      const delId = 1;
       for (let i = historyCapacity; i < recordCount; i++) {
         const record = historyContainer.children[delId];
         historyContainer.removeChild(record);
@@ -67,13 +71,34 @@ const index = (() => {
   };
   
   const keyEH = (key) => {
+    // verify that it is valid and that requires some context
+    // because i want to prevent adding a symbol when there is no digit in the LHS
+    // i need to ensure the user can only type one symbol. that they cannot type symbols until there is a digit
+    // are there any other explicit checks? oh frick i forgot dots too
+    
+    // actually. let them have it. we got the error to say when they do, and the means to check when they do try.
+
+
     delBelow();
 
+    
+    // this is where we check
+    // we first see if it's an operator. then we check if it's in a compatible form
+    // if so: we compute it with calculator logic
+    // if it fails to compute, display given error message or default syntax error
+    // otherwise: we do below. 
+    
     const copy = document.createElement("div");
     copy.innerHTML = curInnerHTML + key;
     historyContainer.appendChild(copy);
     recordCount++;
     scrollEH("ArrowDown");
+
+    // say we had an error message
+    // so it's flashing
+    // when user presses a key and gets here. we know they changed it
+    // or at least isn't computing it again
+    // so we can turn off the error message here
 
     delAbove();
   };
@@ -87,6 +112,8 @@ const index = (() => {
     recordCount++;
     scrollEH("ArrowDown");
 
+    // remove error message here too if it's flashing
+
     delAbove();
   }
 
@@ -99,12 +126,15 @@ const index = (() => {
     } else if (key === "Backspace") {
       delEH();
     } else if (key === "Enter") {
-
+      // in a separate function but basically:
+      // delBelow()
+      // check if of the computableForm. if so, compute
+      // otherwise: ensure it's a valid digit, then we just copy it
+      // otherwise pull up syntax error
     }
   };
 
   window.addEventListener("DOMContentLoaded", () => {
-    scrollEH("ArrowDown"); // temp
     window.addEventListener("keydown", inputEH);
   });
 
@@ -113,10 +143,15 @@ const index = (() => {
 
 /*
 
+- reference error html
+- complete Enter logic
+- add the way to remove the error message
+- figure out how to communicate between different files
 - create separate file for doing the logic of computing 
+- we will use split on it and guarantee correct form
+- finish the parsing and computing of it
+- connect it together and test
+- add the edge case like divide by zero with snarky message
 
-- let's get the actual calculator logic working
-- let's address the error handling
-- let's add decimal points to the mix
 
 */
